382501e3a4dd7405b8a7d1cc81dfd59f4a646484
ashutoshPanda <iamashutoshpanda@gmail.com>
2023-04-22 06:39:16 +0530

    ADD: delete route and handle 404

--- src/routes/tasksInfo.js
+++ src/routes/tasksInfo.js
@@ -9,15 +9,16 @@ const getId = require("../helpers/idGenerator");
 tasksRoutes.use(bodyParser.urlencoded({ extended: false }));
 tasksRoutes.use(bodyParser.json());
 
+const WRITE_PATH = path.join(__dirname, "..", "tasks.json");
+
 tasksRoutes.post("/", (req, res) => {
   const taskDetails = req.body;
-  const writePath = path.join(__dirname, "..", "tasks.json");
   const validationResult = validator.validateTaskInfo(taskDetails, taskData);
   if (validationResult.status) {
     const taskDetailsWithId = { id: getId(), taskDetails };
     const taskDataModified = JSON.parse(JSON.stringify(taskData));
     taskDataModified.task.push(taskDetailsWithId);
-    fs.writeFileSync(writePath, JSON.stringify(taskDataModified), {
+    fs.writeFileSync(WRITE_PATH, JSON.stringify(taskDataModified), {
       encoding: "utf8",
       flag: "w",
     });
@@ -29,18 +30,46 @@ tasksRoutes.post("/", (req, res) => {
   }
 });
 
+function getTaskById(req, res, next) {
+  const id = req.params.id;
+  try {
+    const retrievedTask = taskData.task;
+    const taskIdPassed = req.params.taskId;
+    const taskList = retrievedTask.filter((val) => val.id == taskIdPassed);
+    if (taskList.length === 0) {
+      return res.status(404).json({ message: "Cannot find task" });
+    }
+    req.task = taskList[0];
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  next();
+}
+
 tasksRoutes.get("/", (req, res) => {
   res.status(200);
   res.send(taskData);
 });
 
-tasksRoutes.get("/:taskId", (req, res) => {
-  const retrievedTask = taskData.task;
-  const taskIdPassed = req.params.taskId;
-  const result = retrievedTask.filter((val) => val.id == taskIdPassed);
+tasksRoutes.get("/:taskId", getTaskById, (req, res) => {
+  res.status(200);
+  res.send(req.task);
+});
+
+tasksRoutes.delete("/:taskId", getTaskById, (req, res) => {
+  const taskDataModified = JSON.parse(JSON.stringify(taskData));
+  console.log(taskDataModified);
+  const filteredTasks = taskDataModified.task.filter(
+    (val) => val.id !== req.task.id
+  );
+  taskDataModified.task = filteredTasks;
+  fs.writeFileSync(WRITE_PATH, JSON.stringify(taskDataModified), {
+    encoding: "utf8",
+    flag: "w",
+  });
 
   res.status(200);
-  res.send(result);
+  res.send(filteredTasks);
 });
 
 module.exports = tasksRoutes;
