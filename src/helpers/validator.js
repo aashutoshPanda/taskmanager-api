f4d57ed3e23a1db1eb700c73f6cafb9fa1636ae5
ashutoshPanda <iamashutoshpanda@gmail.com>
2023-04-22 09:05:14 +0530

    ADD: priority with validation

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -1,9 +1,25 @@
 class validator {
+  static validatePriority(taskInfo) {
+    if (taskInfo.hasOwnProperty("priority")) {
+      const passedPriority = taskInfo.priority;
+      if (!typeof passedPriority === "string") {
+        return false;
+      }
+      const ALLOWED_PRIORITY = ["LOW", "MEDIUM", "HIGH"];
+      return ALLOWED_PRIORITY.includes(passedPriority);
+    } else {
+      return true;
+    }
+  }
   static validateTaskInfo(taskInfo, taskData) {
     if (
       taskInfo.hasOwnProperty("title") &&
       taskInfo.hasOwnProperty("description") &&
-      taskInfo.hasOwnProperty("isComplete")
+      taskInfo.hasOwnProperty("isComplete") &&
+      typeof taskInfo.title === "string" &&
+      typeof taskInfo.description === "string" &&
+      typeof taskInfo.isComplete === "boolean" &&
+      this.validatePriority(taskInfo)
     ) {
       return {
         status: true,
